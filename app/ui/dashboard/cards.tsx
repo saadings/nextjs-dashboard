b66b2c258c048bdcc6dd4968bcfccb7236585e67
saadings <saadijutt007@gmail.com>
2023-10-30 14:37:38 +0500

    Update cards.tsx

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -3,9 +3,9 @@ import {
   ClockIcon,
   UserGroupIcon,
   InboxIcon,
-} from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data';
+} from "@heroicons/react/24/outline";
+import { lusitana } from "@/app/ui/fonts";
+import { fetchCardData } from "@/app/lib/data";
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -15,7 +15,6 @@ const iconMap = {
 };
 
 export default async function Cards() {
-
   const {
     numberOfCustomers,
     numberOfInvoices,
@@ -46,7 +45,7 @@ export function Card({
 }: {
   title: string;
   value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
+  type: "invoices" | "customers" | "pending" | "collected";
 }) {
   const Icon = iconMap[type];
 
@@ -54,7 +53,7 @@ export function Card({
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
       <div className="flex p-4">
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-        <h3 className="ml-2 text-sm font-medium">{title}</h3>
+        <h1 className="ml-2 text-sm font-medium">{title}</h1>
       </div>
       <p
         className={`${lusitana.className}
